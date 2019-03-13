1ebcfb81a665d6386faf536066b02b760de4df0e
klandon <klandon@none>
2019-03-13 19:34:41 -0400

    infra init

---
+++ k8sbootstrap/deploy/local.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+kubectl config use-context docker-for-desktop
+kubectl apply -f templates/admin-creation.yaml
+kubectl apply -f templates/storagefix.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb.yaml
+helm init
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
