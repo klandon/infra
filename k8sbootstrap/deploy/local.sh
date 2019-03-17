76f7eeb8fec1f433b1330110d9c27197f1efc227
landonkt <landonkt@none.com>
2019-03-17 10:21:16 -0400

    settig deployment for hello world

--- k8sbootstrap/deploy/local.sh
+++ k8sbootstrap/deploy/local.sh
@@ -6,3 +6,4 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb.yaml
 helm init
 kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
+kubectl apply -f templates/hello-world-example.yaml
